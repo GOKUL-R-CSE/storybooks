2e15511f5b8d66b11f08c5c6447523c52e93af1c
GOKUL_R_CSE <gokulr.cse2020@citchennai.net>
2023-01-29 21:48:59 +0530

    add feature

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const port = process.env.PORT || 5000
 app.listen(port, console.log(`sever running in ${process.env.NODE_ENV} mode on port ${port}`))
