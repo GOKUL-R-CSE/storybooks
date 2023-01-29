4fa3935f2ce9af7fb75f286e39da3a1ef7924b07
GOKUL_R_CSE <gokulr.cse2020@citchennai.net>
2023-01-29 15:06:27 +0530

    google authenticate

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // load config
 dotenv.config({ path: './config/config.env' })
 
+// passport config
+require('./config/passport')(passport)
+
 connectDB()
 
 const app = express()
 
-// logginf
+// logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
@@ -23,11 +28,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
+// session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const port = process.env.PORT || 5000
 app.listen(port, console.log(`sever running in ${process.env.NODE_ENV} mode on port ${port}`))
